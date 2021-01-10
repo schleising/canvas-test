b2baf4e2f70bc520328516b1f94e2e707a619944
schleising <stephen@schleising.net>
2021-01-10 17:39:53 +0000

    Draw a rectangle

---
+++ canvas.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/schleising/canvas"
+)
+
+func main() {
+	canvas := canvas.Initialise("canvas")
+
+	canvas.SetFillStyle("FF8800")
+
+	canvas.FillRect(10, 20, 1000, 200)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/schleising/canvas-test
+
+go 1.16
+
+require github.com/schleising/canvas v0.0.0-20210110173038-51237ddbdc97
